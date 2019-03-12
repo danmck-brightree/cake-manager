615523d870bb7551a41c1dfbe9ba31f6db3bb602
Daniel McKenzie <dmckenzie@brightree.com>
2019-03-12 11:49:32 +0000

    Add bootstrap js and dependencies.

--- CakeManager.Shared/Constants.cs
+++ CakeManager.Shared/Constants.cs
@@ -4,6 +4,7 @@ namespace CakeManager.Shared
 {
     public static class Constants
     {
+        public const int CakeMarkTallyMax = 3;
         public const string LoginRoute = "/login";
         public static readonly Guid TemporaryUserId = new Guid("2234A4E3-7AE6-4882-B204-81F38A3D7D47");
         public static readonly string TemporaryToken = "21B4F518-1B25-47B5-BF5A-8B17E7B2FB51";
